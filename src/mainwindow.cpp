64e631c71cae35d4f152aedc9c75955b59459283
Carina Tsvetkova <carina.prelude@gmail.com>
2012-09-29 16:15:31 +0300

    Add button with Hello message.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "qmessagebox.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,11 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox mb;
+    mb.setText("Hello Kanji Tools!!!");
+    mb.exec();
 }
