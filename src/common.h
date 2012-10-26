2d60fe50ea22b3d101b39253739ba0a183ca314d
Komov Sergej <mihaly489@gmail.com>
2012-10-26 10:34:15 +0300

    Add missing header.

---
+++ src/common.h
@@ -0,0 +1,45 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <string>
+
+
+enum
+{
+    ACCOUNT_TYPE_ERROR=0,
+    ACCOUNT_TYPE_ADMIN=1,
+    ACCOUNT_TYPE_TEACHER,
+    ACCOUNT_TYPE_STUDENT
+};
+
+struct kanji_t
+{
+    std::string kanji,on_youmi,kun_youmi,meaning,examples;
+    kanji_t(std::string kanji,std::string on_youmi,std::string kun_youmi,std::string meaning,std::string examples)
+    {
+        this->kanji = kanji;
+        this->on_youmi = on_youmi;
+        this->kun_youmi = kun_youmi;
+        this->meaning = meaning;
+        this->examples = examples;
+    }
+};
+
+struct user_t
+{
+    std::string id,name,surename,login,password;
+    int account_type;
+};
+
+struct case_t
+{
+    std::string variants[4];
+    std::string question;
+};
+
+struct test_result_t
+{
+    std::string id,test_type,test_id,person_id;
+};
+
+#endif
