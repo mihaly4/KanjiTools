5bb5a4aa3b70977502b1cd21a7842a314944bed0
Sergej Komov <mihaly489@gmail.com>
2012-10-27 11:01:36 +0300

    Forget some files.

---
+++ src/connector_wraper.h
@@ -0,0 +1,56 @@
+#ifndef CONNECTOR_WRAPER_H
+#define CONNECTOR_WRAPER_H
+class QSqlDatabase;
+class QSqlQuery;
+class QSqlResult;
+namespace sql
+{
+
+    class SQLException
+    {
+    public:
+        std::string what();
+    };
+
+    class ResultSet
+    {
+        QSqlQuery * res;
+    public:
+        ResultSet(QSqlQuery * res)
+        {
+            this->res = res;
+        }
+
+        bool next();
+        std::string getString(int idx);
+        int getInt(int idx);
+        int rowsCount();
+    };
+	class Statement
+	{
+        QSqlQuery * qr;
+    public:
+        ResultSet * executeQuery(std::string query);
+	};
+    class Connection
+    {
+        ::QSqlDatabase *  db;
+    public:
+        Connection(::QSqlDatabase *  db)
+        {
+            this->db = db;
+        }
+
+        void setSchema(std::string name);
+        Statement * createStatement();
+    };
+	class Driver
+	{
+        ::QSqlDatabase *  db;
+	public:
+        Driver();
+		Connection * connect(std::string host,std::string login,std::string pass);
+	};
+};
+sql::Driver * get_driver_instance();
+#endif
---
+++ src/connector_wrapper.cpp
@@ -0,0 +1,75 @@
+#include <string>
+#include "connector_wraper.h"
+#include <QtSql>
+#include <QSqlDatabase>
+#include <QMessageBox>
+
+
+sql::Driver * get_driver_instance()
+{
+    return new sql::Driver();
+}
+
+QSqlDatabase ololo;
+sql::Driver::Driver()
+{
+    ololo =::QSqlDatabase::addDatabase("QMYSQL");
+    db = & ololo;
+
+}
+sql::Connection * sql::Driver::connect(std::string host, std::string login, std::string pass)
+{
+    db->setHostName(QString::fromStdString(host));
+    db->setUserName(QString::fromStdString(login));
+    db->setPassword(QString::fromStdString(pass));
+    return new sql::Connection(db);
+}
+
+void sql::Connection::setSchema(std::string name)
+{
+    db->setDatabaseName(QString::fromStdString(name));
+
+    if (!db->open())
+    {
+      QMessageBox::critical(0, QObject::tr("Database Error"),
+                db->lastError().text());
+    }
+}
+
+sql::Statement * sql::Connection::createStatement()
+{
+    return new sql::Statement();
+}
+
+sql::ResultSet * sql::Statement::executeQuery(std::string query)
+{
+    qr = new QSqlQuery(QString::fromStdString(query));
+    return new sql::ResultSet(qr);
+
+}
+
+bool sql::ResultSet::next()
+{
+    return res->next();
+}
+
+std::string sql::ResultSet::getString(int idx)
+{
+    return res->value(idx-1).toString().toStdString();
+}
+
+int sql::ResultSet::getInt(int idx)
+{
+    return res->value(idx-1).toInt();
+}
+
+int sql::ResultSet::rowsCount()
+{
+    return res->size();
+}
+
+std::string sql::SQLException::what()
+{
+    return "this should be fixed...";
+}
+
